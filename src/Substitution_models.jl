589b349de03186e4cc97f193954e61a741b0aed9
Justin Angevaare <jangevaa@uoguelph.ca>
2015-05-08 13:02:57 -0400

    work on substitution models

--- src/Substitution_models.jl
+++ src/Substitution_models.jl
@@ -1,11 +1,35 @@
-function JC69(p::Tuple)
+function JC69(θ::Tuple, time)
   """
-  Returns the JC69 (Jukes and Cantor, 1969) substitution rate matrix with parameter μ
+  Returns the JC69 (Jukes and Cantor, 1969) transition matrix with parameter μ, the overall substitution rate. This model assumes equal base frequencies and mutation rates.
+  http://en.wikipedia.org/wiki/Models_of_DNA_evolution#Most_common_models_of_DNA_evolution
   """
-  @assert 0 < p[1] ["Invalid parameter value"]
-  [[0      p[1]/4  p[1]/4  p[1]/4]
-   [p[1]/4 0       p[1]/4  p[1]/4]
-   [p[1]/4 p[1]/4  0       p[1]/4]
-   [p[1]/4 p[1]/4  p[1]/4  0]]
+  μ = θ[1]
+  p_0 = 0.25 + 0.75*e^(-time*μ)
+  p_1 = 0.25 - 0.25*e^(-time*μ)
+  return [[p_0 p_1 p_1 p_1]
+          [p_1 p_0 p_1 p_1]
+          [p_1 p_1 p_0 p_1]
+          [p_1 p_1 p_1 p_0]]
 end
 
+function K80(θ::Tuple, time)
+  """
+  Returns the K80 (Kimura et al., 1980) transition matrix with parameters α and β; the transition and transversion rate parameters respectively. This model assumes equal base frequencies and unique transition and transversion rates
+  http://en.wikipedia.org/wiki/Models_of_DNA_evolution#Most_common_models_of_DNA_evolution
+  """
+  α = θ[1]
+  β = θ[2]
+  p_0 = 0.25 + 0.25*e^(-4*β*time) + 0.5*e^(-2*(α + β)*time)
+  p_1 = 0.25 + 0.25*e^(-4*β*time) - 0.5*e^(-2*(α + β)*time)
+  p_2 = 0.25 - 0.25*e^(-4*β*time)
+  return [[p_0 p_1 p_2 p_2]
+          [p_1 p_0 p_2 p_2]
+          [p_2 p_2 p_0 p_1]
+          [p_2 p_2 p_1 p_0]]
+end
+
+function F81(θ::Tuple, time)
+   """
+  Returns the F81 (Felsenstein, 1980) transition matrix with parameters μ, π_A, π_G, π_T, and π_C, which are the overall substitution rate and the base specific frequencies respectively.
+  http://en.wikipedia.org/wiki/Models_of_DNA_evolution#Most_common_models_of_DNA_evolution
+  """
