619650d822a7b28cffc31066f77204973d663f21
Justin Angevaare <jangevaa@uoguelph.ca>
2015-05-19 14:29:21 -0400

    add a test

--- test/runtests.jl
+++ test/runtests.jl
@@ -10,4 +10,5 @@ using Base.Test
 @test nucleotide_revert([1,2,3,4]) == ["A", "G", "C", "U"]
 
 test_sequence=generate_sequence(700, 0.25, 0.25, 0.25, 0.25)
+@test length(test_sequence) == 700
 @test sum(test_sequence .!= mutate_sequence(test_sequence, JC69((1,),100))) > 0
