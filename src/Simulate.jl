c87c725a37161f99f93432167c2c7416fca46f2c
Justin Angevaare <jangevaa@uoguelph.ca>
2015-05-25 11:54:49 -0400

    flesh out population creation

--- src/Simulate.jl
+++ src/Simulate.jl
@@ -1,13 +1,22 @@
-function create_population(sequence_init::Vector, covariate_init::Array)
+function create_population(init_seq::Vector, init_var::Array)
 """
 Initialize an infection database
 """
-  # infection times, infection sources, recovery times, covariate times, sequence times
-  events = Array([],[],[],[],[]),
+  # identifier, infection times, infection sources, recovery times, covariate times, sequence times
+  events = Array[Array[[0], [NaN],  [NaN],  [NaN],  [NaN], [0]],
+                 Array[[1], [],     [],     [],     [0],   []]]
 
   # covariate history, sequence history
-  history = Array([[ ], [ ]],[[ ], [ ]])
+  history = Array[Array[[[NaN, NaN]],     [init_seq]],
+                  Array[[[init_var[1,:]]],[]]]
 
+  # push individuals to these arrays.
+  for r = 2:shape(init_var,1)
+    push!(events, Array[[r], [],     [],     [],     [0],   []])
+    push!(history, Array[[[init_var[r,:]]],[]])
+  end
+
+  # save as a population object type
   return population(events, history)
 end
 
