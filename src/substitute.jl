582f7b37e4c27dab1fc4a6a00c9b04209ba9de1e
Justin Angevaare <jangevaa@uoguelph.ca>
2015-05-26 14:48:47 -0400

    work with rate matrices instead of transition matrices

--- src/substitute.jl
+++ src/substitute.jl
@@ -1,32 +1,43 @@
-function JC69(θ::Tuple, time)
+function JC69(θ::Tuple)
   """
-  Returns the JC69 (Jukes and Cantor, 1969) transition matrix with parameter μ, the overall substitution rate. This model assumes equal base frequencies and mutation rates.
-  http://en.wikipedia.org/wiki/Models_of_DNA_evolution#Most_common_models_of_DNA_evolution
+  Returns the JC69 Q matrix with parameter λ
   """
-  μ = θ[1]
-  p_0 = 0.25 + 0.75*e^(-time*μ)
-  p_1 = 0.25 - 0.25*e^(-time*μ)
-  return [[p_0 p_1 p_1 p_1]
-          [p_1 p_0 p_1 p_1]
-          [p_1 p_1 p_0 p_1]
-          [p_1 p_1 p_1 p_0]]
+  λ = θ[1]
+  return [[NaN λ λ λ]
+          [λ NaN λ λ]
+          [λ λ NaN λ]
+          [λ λ λ NaN]]
 end
 
-function K80(θ::Tuple, time)
-  """
-  Returns the K80 (Kimura et al., 1980) transition matrix with parameters α and β; the transition and transversion rate parameters respectively. This model assumes equal base frequencies and unique transition and transversion rates
-  http://en.wikipedia.org/wiki/Models_of_DNA_evolution#Most_common_models_of_DNA_evolution
-  """
-  α = θ[1]
-  β = θ[2]
-  p_0 = 0.25 + 0.25*e^(-4*β*time) + 0.5*e^(-2*(α + β)*time)
-  p_1 = 0.25 + 0.25*e^(-4*β*time) - 0.5*e^(-2*(α + β)*time)
-  p_2 = 0.25 - 0.25*e^(-4*β*time)
-  return [[p_0 p_1 p_2 p_2]
-          [p_1 p_0 p_2 p_2]
-          [p_2 p_2 p_0 p_1]
-          [p_2 p_2 p_1 p_0]]
-end
+# function JC69(θ::Tuple, time)
+#   """
+#   Returns the JC69 (Jukes and Cantor, 1969) transition matrix with parameter μ, the overall substitution rate. This model assumes equal base frequencies and mutation rates.
+#   http://en.wikipedia.org/wiki/Models_of_DNA_evolution#Most_common_models_of_DNA_evolution
+#   """
+#   μ = θ[1]
+#   p_0 = 0.25 + 0.75*e^(-time*μ)
+#   p_1 = 0.25 - 0.25*e^(-time*μ)
+#   return [[p_0 p_1 p_1 p_1]
+#           [p_1 p_0 p_1 p_1]
+#           [p_1 p_1 p_0 p_1]
+#           [p_1 p_1 p_1 p_0]]
+# end
+#
+# function K80(θ::Tuple, time)
+#   """
+#   Returns the K80 (Kimura et al., 1980) transition matrix with parameters α and β; the transition and transversion rate parameters respectively. This model assumes equal base frequencies and unique transition and transversion rates
+#   http://en.wikipedia.org/wiki/Models_of_DNA_evolution#Most_common_models_of_DNA_evolution
+#   """
+#   α = θ[1]
+#   β = θ[2]
+#   p_0 = 0.25 + 0.25*e^(-4*β*time) + 0.5*e^(-2*(α + β)*time)
+#   p_1 = 0.25 + 0.25*e^(-4*β*time) - 0.5*e^(-2*(α + β)*time)
+#   p_2 = 0.25 - 0.25*e^(-4*β*time)
+#   return [[p_0 p_1 p_2 p_2]
+#           [p_1 p_0 p_2 p_2]
+#           [p_2 p_2 p_0 p_1]
+#           [p_2 p_2 p_1 p_0]]
+# end
 
 # function HKY85(θ::Tuple, time)
 #   """
