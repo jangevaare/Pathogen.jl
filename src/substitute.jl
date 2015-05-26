f8508b4e6b781e982a46752df638f1f368cd8039
Justin Angevaare <jangevaa@uoguelph.ca>
2015-05-26 14:39:03 -0400

    rename...

---
+++ src/substitute.jl
@@ -0,0 +1,41 @@
+function JC69(θ::Tuple, time)
+  """
+  Returns the JC69 (Jukes and Cantor, 1969) transition matrix with parameter μ, the overall substitution rate. This model assumes equal base frequencies and mutation rates.
+  http://en.wikipedia.org/wiki/Models_of_DNA_evolution#Most_common_models_of_DNA_evolution
+  """
+  μ = θ[1]
+  p_0 = 0.25 + 0.75*e^(-time*μ)
+  p_1 = 0.25 - 0.25*e^(-time*μ)
+  return [[p_0 p_1 p_1 p_1]
+          [p_1 p_0 p_1 p_1]
+          [p_1 p_1 p_0 p_1]
+          [p_1 p_1 p_1 p_0]]
+end
+
+function K80(θ::Tuple, time)
+  """
+  Returns the K80 (Kimura et al., 1980) transition matrix with parameters α and β; the transition and transversion rate parameters respectively. This model assumes equal base frequencies and unique transition and transversion rates
+  http://en.wikipedia.org/wiki/Models_of_DNA_evolution#Most_common_models_of_DNA_evolution
+  """
+  α = θ[1]
+  β = θ[2]
+  p_0 = 0.25 + 0.25*e^(-4*β*time) + 0.5*e^(-2*(α + β)*time)
+  p_1 = 0.25 + 0.25*e^(-4*β*time) - 0.5*e^(-2*(α + β)*time)
+  p_2 = 0.25 - 0.25*e^(-4*β*time)
+  return [[p_0 p_1 p_2 p_2]
+          [p_1 p_0 p_2 p_2]
+          [p_2 p_2 p_0 p_1]
+          [p_2 p_2 p_1 p_0]]
+end
+
+# function HKY85(θ::Tuple, time)
+#   """
+#   Returns the HKY85 (Hasegawa, Kishino and Yano 1985) transition matrix with parameters α, β, π_A, π_G, π_C, and π_T, which are the the transition and transversion rate parameters and the base specific frequencies respectively.
+#   """
+#   α    = θ[1]
+#   β    = θ[2]
+#   π_A  = θ[3]
+#   π_G  = θ[4]
+#   π_C  = θ[5]
+#   π_T  = θ[6]
+# end
---
+++ src/utilities.jl
@@ -0,0 +1,57 @@
+"""
+Sequence.jl - basic utilities for dealing with sequence data
+Justin Angevaare
+May 2015
+"""
+
+function nucleotide_convert(x::ASCIIString)
+"""
+Convert a sequence of nucleotide bases into an integer representation
+"""
+  sequence = fill(0, length(x))
+  for i = 1:length(x)
+    if x[i] == 'A' sequence[i] = 1 end
+    if x[i] == 'G' sequence[i] = 2 end
+    if x[i] == 'C' sequence[i] = 3 end
+    if x[i] == 'U' sequence[i] = 4 end
+  end
+  return sequence
+end
+
+function nucleotide_revert(x::Vector{Int})
+"""
+Convert an integer sequence into a nucleotide base representation
+"""
+  sequence = fill("Z", length(x))
+  for i = 1:length(x)
+    if x[i] == 1 sequence[i] = "A" end
+    if x[i] == 2 sequence[i] = "G" end
+    if x[i] == 3 sequence[i] = "C" end
+    if x[i] == 4 sequence[i] = "U" end
+  end
+sequence
+end
+
+function generate_sequence(n::Int, π_A::Float64, π_G::Float64, π_C::Float64, π_U::Float64)
+"""
+Generate a nucleotide sequence of length `n`, with specific nucleotide frequencies
+"""
+  @assert(sum([π_A, π_G, π_C, π_U]) == 1, "Nucleotide frequencies must sum to 1")
+  @assert(all(0 .< [π_A, π_G, π_C, π_U] .< 1), "Each nucleotide frequency must be between 0 and 10")
+  sequence = fill(0, n)
+  for i = 1:n
+    sequence[i]=find(rand(Multinomial(1, [π_A, π_G, π_C, π_U])))[1]
+  end
+  return sequence
+end
+
+function mutate_sequence(sequence::Vector, substitution_matrix::Array)
+  """
+  Mutate a nucleotide sequence based on a specified substitution matrix
+  """
+  new_sequence = fill(0, length(sequence))
+  for i in 1:length(new_sequence)
+    new_sequence[i] = find(rand(Multinomial(1, substitution_matrix[sequence[i],:][:])))[1]
+  end
+  return new_sequence
+end
