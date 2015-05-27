8110a5f75e4d6c28dd0dd69b9aaa4b09de454195
Justin Angevaare <jangevaa@uoguelph.ca>
2015-05-27 11:57:59 -0400

    remove old tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -6,9 +6,6 @@ May 2015
 using Pathogen
 using Base.Test
 
-@test nucleotide_convert("AGCU") == [1, 2, 3, 4]
-@test nucleotide_revert([1,2,3,4]) == ["A", "G", "C", "U"]
-
 test_sequence=generate_sequence(700, 0.25, 0.25, 0.25, 0.25)
 @test length(test_sequence) == 700
-@test sum(test_sequence .!= mutate_sequence(test_sequence, JC69((1,),100))) > 0
+
