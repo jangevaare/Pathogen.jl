df7f7011e3b1962a64b2bbd8a3134b224fade3ee
Justin Angevaare <jangevaa@uoguelph.ca>
2015-06-12 18:00:46 -0400

    more conversion functions

--- src/utilities.jl
+++ src/utilities.jl
@@ -5,16 +5,16 @@ May 2015
 """
 import Base.convert
 
-function convert(::Type{Int64}, x::Nucleotide2bitSeq)
+function convert(::Type{Vector{Int64}}, x::Nucleotide2bitSeq)
   """
-  Add a conversion method to move from Nucleotide to an integer
+  Add a conversion method to move from Nucleotide to an integer vector
   """
   return sub2ind((2,2), x.b1 .+1, x.b2 .+1)
 end
 
 function convert(::Type{Nucleotide2bitSeq}, x::Vector{Int64})
   """
-  Add a conversion method to move from an integer to a nucleotide sequence
+  Add a conversion method to move from an integer vector to a nucleotide sequence
   """
   b1,b2 = ind2sub((2,2), x)
   if length(x) == 1
@@ -24,6 +24,21 @@ function convert(::Type{Nucleotide2bitSeq}, x::Vector{Int64})
   end
 end
 
+function convert(::Type{Int64}, x::Nucleotide2bitBase)
+  """
+  Add a conversion method to move from nucleotide base to an integer
+  """
+  return sub2ind((2,2), x.b1 .+1, x.b2 .+1)
+end
+
+function convert(::Type{Nucleotide2bitBase}, x::Int64)
+  """
+  Add a conversion method to move from an integer to a nucleotide base
+  """
+  b1,b2 = ind2sub((2,2), x)
+  return Nucleotide2bitBase(convert(BitArray, [b1 - 1]), convert(BitArray, [b2 - 1]))
+end
+
 function generate_seq(n::Int, π_A::Float64, π_T::Float64, π_C::Float64, π_G::Float64)
   """
   Generate a nucleotide sequence of length `n`, with specific nucleotide frequencies
@@ -31,10 +46,7 @@ function generate_seq(n::Int, π_A::Float64, π_T::Float64, π_C::Float64, π_G:
   @assert(sum([π_A, π_T, π_C, π_G]) == 1, "Nucleotide frequencies must sum to 1")
   @assert(all(0 .< [π_A, π_T, π_C, π_G] .< 1), "Each nucleotide frequency must be between 0 and 1")
   sequence = fill(0, n)
-  for i = 1:n
-    sequence[i]=findfirst(rand(Multinomial(1, [π_A, π_T, π_C, π_G])))
-  end
-  return nucleotide("AGCU"[sequence])
+  return nucleotide("ATCG"[findn(rand(Multinomial(1, [π_A, π_T, π_C, π_G]),n))[1]])
 end
 
 function generate_2bitseq(n::Int, π_A::Float64, π_T::Float64, π_C::Float64, π_G::Float64)
@@ -67,7 +79,6 @@ function plotdata(population, time)
   """
   Create dataframes with all necessary plotting information
   """
-#   states = DataFrame(x = Float64[], y = Float64[], state = ASCIIString[])
   states = DataFrame(id = fill(NaN,4), x = fill(NaN,4), y = fill(NaN,4), state = ["S", "E", "I", "S*"])
   routes = DataFrame(x = Float64[], y = Float64[], age = Float64[])
   for i = 2:length(population.events)
@@ -84,16 +95,15 @@ function plotdata(population, time)
   return states, routes
 end
 
-function geneticdistance(ancestor::Vector{Nucleotide}, descendent::Vector{Nucleotide}, substitution_matrix::Array)
+function geneticdistance(ancestor::Nucleotide2bitSeq, descendent::Nucleotide2bitSeq, substitution_matrix::Array)
   """
   Compute the genetic distance between two nucleotide sequences based on a `substitution_matrix`
   """
   @assert(length(ancestor) == length(descendent), "Sequences must be equal in length")
-  nucleotide_ref = nucleotide("AGCU")
   rate_vector = Float64[]
   for i = 1:length(ancestor)
     if ancestor[i] != descendent[i]
-     push!(rate_vector, substitution_matrix[ancestor[i] .== nucleotide_ref, descendent[i] .== nucleotide_ref])
+     push!(rate_vector, substitution_matrix[convert(ancestor[i]), descendent[i]])
     end
   end
   rate_vector .^= -1
