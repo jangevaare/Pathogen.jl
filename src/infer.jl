92f68862bf5ca7dce30a5784d0e85f6472b983f0
Justin Angevaare <jangevaa@uoguelph.ca>
2015-07-10 23:47:14 -0400

    work on prior function

--- src/infer.jl
+++ src/infer.jl
@@ -41,11 +41,32 @@ function seqdistance(seq1::Nucleotide2bitSeq, seq2::Nucleotide2bitSeq, substitut
   """
 end
 
-function logprior1
+function create_logprior1(α_prior::UnivariateDistribution, β_prior::UnivariateDistribution, ρ_prior::UnivariateDistribution, γ_prior::UnivariateDistribution, η_prior::UnivariateDistribution, ν_prior::UnivariateDistribution)
+  """
+  Create a log prior function using specified prior univariate distributions
+  α, β: powerlaw exposure kernel parameters
+  η: external pressure rate
+  ρ: infectious rate (1/mean latent period)
+  γ: recovery rate (1/mean infectious period)
+  ν: detection rate (1/mean detection lag)
+  """
+  return function(α::Float64, β::Float64, ρ::Float64, γ::Float64, η::Float64, ν::Float64)
+    """
+    α, β: powerlaw exposure kernel parameters
+    η: external pressure rate
+    ρ: infectious rate (1/mean latent period)
+    γ: recovery rate (1/mean infectious period)
+    ν: detection rate (1/mean detection lag)
+    """
+    return
+  end
+end
 
-function augment1a(ρ::Float64, ν::Float64)
+function augment1(ρ::Float64, ν::Float64)
   """
   Augments surveilance data with infectiousness
+  """
+  end
 
 function loglikelihood1(α::Float64, β::Float64, ρ::Float64, γ::Float64, η::Float64, ν::Float64, obs, aug)
   """
@@ -56,6 +77,4 @@ function loglikelihood1(α::Float64, β::Float64, ρ::Float64, γ::Float64, η::
   ν: detection rate (1/mean detection lag)
   """
 end
-
-function augment1b()
 
