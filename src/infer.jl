eb6e54b4ae2fd367d4ff82e4af780536ecab8277
Justin Angevaare <jangevaa@uoguelph.ca>
2015-07-21 12:24:42 -0400

    remove trial and error initialization

--- src/infer.jl
+++ src/infer.jl
@@ -144,25 +144,16 @@ function SEIR_initialize(priors::SEIR_priors, obs::SEIR_events, dist=Euclidean()
   γ: recovery rate (1/mean infectious period)
   ν: detection rate (1/mean detection lag)
   """
-  logposterior = -Inf
-  loopcount = 1
-  while loopcount <= 1000 && logposterior == -Inf
-    α = rand(priors.α)
-    β = rand(priors.β)
-    ρ = rand(priors.ρ)
-    γ = rand(priors.γ)
-    η = rand(priors.η)
-    ν = rand(priors.ν)
-    aug = SEIR_augmentation(ρ, ν, obs)
-    ll, sources = SEIR_loglikelihood(α, β, ρ, γ, η, ν, aug, obs, dist)
-    logposterior = ll + SEIR_logprior(priors, α, β, ρ, γ, η, ν)
-    loopcount += 1
-    if logposterior > -Inf
-      return SEIR_trace([α], [β], [ρ], [γ], [η], [ν], [aug], Array[sources], [logposterior])
-    elseif loopcount == 1000
-      print("Max initialization attempts reached")
-    end
-  end
+  α = rand(priors.α)
+  β = rand(priors.β)
+  ρ = rand(priors.ρ)
+  γ = rand(priors.γ)
+  η = rand(priors.η)
+  ν = rand(priors.ν)
+  aug = SEIR_augmentation(ρ, ν, obs)
+  ll, sources = SEIR_loglikelihood(α, β, ρ, γ, η, ν, aug, obs, dist)
+  logposterior = ll + SEIR_logprior(priors, α, β, ρ, γ, η, ν)
+  return SEIR_trace([α], [β], [ρ], [γ], [η], [ν], [aug], Array[sources], [logposterior])
 end
 
 function SEIR_MCMC(n::Int64, transition_cov::Array{Float64}, trace::SEIR_trace, priors::SEIR_priors, obs::SEIR_events, dist=Euclidean())
