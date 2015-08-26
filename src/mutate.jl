dd62c45b4c8bc75dbefa67e124aefb2bb27077f5
Justin Angevaare <jangevaa@uoguelph.ca>
2015-08-26 12:46:58 -0400

    generalize substitution to mutatation

---
+++ src/mutate.jl
@@ -0,0 +1,36 @@
+"""
+mutate.jl - mutation rate functions
+May 2015
+"""
+
+function jc69(θ::Tuple)
+  """
+  Returns the JC69 Q matrix with parameter λ
+  """
+  λ = θ[1]
+  return [[0 λ λ λ]
+          [λ 0 λ λ]
+          [λ λ 0 λ]
+          [λ λ λ 0]]
+end
+
+# function k80(θ::Tuple, time)
+#   """
+#   Returns the K80 (Kimura et al., 1980) transition matrix with parameters α and β; the transition and transversion rate parameters respectively. This model assumes equal base frequencies and unique transition and transversion rates
+#   http://en.wikipedia.org/wiki/Models_of_DNA_evolution#Most_common_models_of_DNA_evolution
+#   """
+#   α = θ[1]
+#   β = θ[2]
+# end
+
+# function hky85(θ::Tuple, time)
+#   """
+#   Returns the HKY85 (Hasegawa, Kishino and Yano 1985) transition matrix with parameters α, β, π_A, π_T, π_C, and π_G, which are the the transition and transversion rate parameters and the base specific frequencies respectively.
+#   """
+#   α    = θ[1]
+#   β    = θ[2]
+#   π_A  = θ[3]
+#   π_T  = θ[4]
+#   π_C  = θ[5]
+#   π_G  = θ[6]
+# end
