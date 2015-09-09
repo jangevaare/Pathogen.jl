d77072dc76167dd16a3003b6fce2e2b53577ad45
Justin Angevaare <jangevaa@uoguelph.ca>
2015-09-09 15:12:30 -0400

    network_loglikelihood fix

--- src/infer.jl
+++ src/infer.jl
@@ -132,8 +132,8 @@ function seq_distances(obs::SEIR_observed, aug::SEIR_augmented, network::Array, 
 
   seq_dist = fill(0., (size(network, 2), size(network, 2)))
 
-  for i = length(infected)
-    for j = 1:i
+  for i = 1:length(infected)
+    for j = 1:(i-1)
 
       k = 1
       while length(pathways[i]) > k && length(pathways[j]) > k && pathways[i][end - k] == pathways[j][end - k]
@@ -198,15 +198,15 @@ function network_loglikelihood(obs::SEIR_observed, aug::SEIR_augmented, network:
   Loglikelihood for an entire transmission network
   """
   ll = 0.
+  infected = find(isseq(obs.seq))
   seq_dist = seq_distances(obs, aug, network, debug)
-  for i = 1:size(seq_dist, 1)
-    for j = 1:i
-       ll += seq_loglikelihood(obs.seq[i], obs.seq[j], seq_dist[i,j], substitution_matrix)
+  for i = 1:length(infected)
+    for j = 1:(i-1)
+       ll += seq_loglikelihood(obs.seq[infected[i]], obs.seq[infected[j]], seq_dist[i,j], substitution_matrix)
     end
   end
   return ll
 end
-
 
 function SEIR_loglikelihood(α::Float64, β::Float64, η::Float64, ρ::Float64, γ::Float64, aug::SEIR_augmented, obs::SEIR_observed, dist::Metric, debug=false::Bool)
   """
