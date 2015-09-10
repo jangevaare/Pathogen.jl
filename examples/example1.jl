d35d18d0193c5dbe4960212e3df00e1af1f56e74
Justin Angevaare <jangevaa@uoguelph.ca>
2015-09-10 00:19:59 -0400

    example fixes

--- examples/example1.jl
+++ examples/example1.jl
@@ -37,10 +37,10 @@ ilm_priors = SEIR_priors(Uniform(0,10), Uniform(0,10), Uniform(0,0.01), Uniform(
 detection_priors = Lag_priors(Uniform(1,3))
 mutation_priors = JC69_priors(Uniform(0,0.1))
 
-ilm_trace, detection_trace, mutation_trace = MCMC(100000, diagm([0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5]), ilm_priors, detection_priors, mutation_priors, obs)
+ilm_trace, detection_trace, mutation_trace = MCMC(100000, diagm([1., 1., 1., 1., 1., 1., 1.]), ilm_priors, detection_priors, mutation_priors, obs)
 
-# Tune the transition kernel's covariance matrix over 300k iterations
-n = 300
+# Tune the transition kernel's covariance matrix over 200k iterations
+n = 200
 for i = 1:n
 
   # Progress bar
@@ -54,11 +54,11 @@ for i = 1:n
   opt_cov = cov([ilm_trace.α ilm_trace.β ilm_trace.ρ ilm_trace.γ ilm_trace.η detection_trace.ν mutation_trace.λ])*(2.38^2)/7.
 
   # Perform 1000 MCMC iterations
-  SEIR_MCMC(1000, opt_cov, trace, priors, obs, false)
+  MCMC(1000, opt_cov, ilm_trace, detection_trace, mutation_trace, ilm_priors, detection_priors, mutation_priors, obs, false, false)
 end
 
 opt_cov = cov([ilm_trace.α ilm_trace.β ilm_trace.ρ ilm_trace.γ ilm_trace.η detection_trace.ν mutation_trace.λ])*(2.38^2)/7.
-SEIR_MCMC(100000, opt_cov, trace, priors, obs)
+MCMC(100000, opt_cov, ilm_trace, detection_trace, mutation_trace, ilm_priors, detection_priors, mutation_priors, obs, false)
 
 # Simulation/Maximum posteriori visualization
 images = 500
@@ -70,7 +70,7 @@ for time = 1:images
             layer(routes, x="x", y="y", group="age", Geom.polygon),
             Theme(panel_opacity=1., panel_fill=color("white"), default_color=color("black"), background_color=color("white")))
 
-  states, routes = plotdata(obs, trace, max_tracelp, (time*maximum([maximum(ilm_trace.aug[max_tracelp]), pop.timeline[1][end]])/images))
+  states, routes = plotdata(obs, ilm_trace, max_tracelp, (time*maximum([maximum(ilm_trace.aug[max_tracelp]), pop.timeline[1][end]])/images))
   p2 = plot(layer(states, x="x", y="y", color="state", Geom.point),
             layer(routes, x="x", y="y", group="age", Geom.polygon),
             Theme(panel_opacity=1., panel_fill=color("white"), default_color=color("black"), background_color=color("white")))
@@ -123,7 +123,7 @@ draw(PNG("SEIR_traceplot.png", 20cm, 15cm),
 # logposterior plot (last 100k iterations)
 draw(PNG("SEIR_logposterior.png", 20cm, 15cm),
      plot(x=1:100000,
-          y=trace.logposterior[end-99999:end],
+          y=ilm_trace.logposterior[end-99999:end],
           Geom.line,
           Theme(panel_opacity=1.,
                 panel_fill=color("white"),
