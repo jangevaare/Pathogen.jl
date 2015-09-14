980584dc41dc4f9849e81d8ef5b50cf275964ab5
Justin Angevaare <jangevaa@uoguelph.ca>
2015-09-14 13:23:52 -0400

    triple check SEIR ll calculation

--- src/infer.jl
+++ src/infer.jl
@@ -252,8 +252,7 @@ function SEIR_loglikelihood(α::Float64, β::Float64, η::Float64, ρ::Float64, 
   α, β: powerlaw exposure kernel parameters
   η: external pressure rate
   ρ: infectivity rate (1/mean latent period)
-  γ: recovery rate (1/mean infectious period)
-  ν: detection rate (1/mean detection lag)
+  γ: removal rate (1/mean infectious period)
   """
   # Initiate an exposure network
   network = fill(false, (1 + length(obs.covariates), length(obs.covariates)))
@@ -276,9 +275,16 @@ function SEIR_loglikelihood(α::Float64, β::Float64, η::Float64, ρ::Float64, 
   # First row is external pressure rate
   rate_array[1,:] = η
 
-  # The rest of the events
+  # Loglikelihood of events that have been observed
   for i = 1:length(event_order)
+
+    # Stop loglikelihood calculation after last event considered
     isnan(event_times[event_order[i]]) && break
+
+    # Stop loglikelihood calculation anytime the loglikelihood goes to -Inf
+    ll == -Inf && break
+
+    # Convert linear index to an event tuple (individual, event type)
     id = ind2sub(size(event_times), event_order[i])
 
     # Don't consider likelilihood contribution of first event
@@ -291,17 +297,21 @@ function SEIR_loglikelihood(α::Float64, β::Float64, η::Float64, ρ::Float64, 
 
     # Exposure event
     if id[2] == 1
+
       # Generate a exposure source based on disease pressures at time of exposure
       network[:, id[1]] = rand(Multinomial(1, rate_array[1:(length(obs.covariates)+1), id[1]]./sum(rate_array[1:(length(obs.covariates)+1), id[1]])))
-      # Update rate array (exposure rates, latent period)
+
+      # Update exposure and infectivity rates
       rate_array[:, id[1]] = 0.
       rate_array[1 + size(rate_array, 2) + 1, id[1]] = ρ
 
     # Infectiousness event
     elseif id[2] == 2
-      # Update rate_array for latent and infectious periods
-      rate_array[1 + size(rate_array, 2) + 1, id[1]] = 0.
+
+      # Update infectivity and removal rates
+      rate_array[:, id[1]] = 0.
       rate_array[1 + size(rate_array, 2) + 2, id[1]] = γ
+
       # Update exposure rates for rest of susceptible population
       for j = 1:size(rate_array, 2)
         if j != id[1] && rate_array[1, j] != 0.
@@ -313,12 +323,13 @@ function SEIR_loglikelihood(α::Float64, β::Float64, η::Float64, ρ::Float64, 
 
     # Removal event
     elseif id[2] == 3
-      # Update rate_array for exposure & removal
-      rate_array[id[1] + 1, :] = 0.
+
+      # Update removal and exposure rates
       rate_array[:, id[1]] = 0.
+      rate_array[id[1] + 1, :] = 0.
     end
 
-    # Provide loop position when loglikelihood goes to -Inf if desired
+    # Provide loop position when loglikelihood goes to -Inf when debugging
     if debug && ll == -Inf
       if id[2] == 1
         print("Event $i (exposure of individual $(id[1])) caused loglikelihood to go to -Inf")
@@ -328,12 +339,6 @@ function SEIR_loglikelihood(α::Float64, β::Float64, η::Float64, ρ::Float64, 
         print("Event $i (removal of individual $(id[1])) caused loglikelihood to go to -Inf")
       end
     end
-
-    # Prevent needless calculation
-    if ll == -Inf
-      break
-    end
-
   end
   return ll, network
 end
