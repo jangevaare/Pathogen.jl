cb55248eb14e450c3868d520931c0325fd224549
Justin Angevaare <jangevaa@uoguelph.ca>
2015-10-01 14:14:08 -0400

    add more debug output

--- examples/example1.jl
+++ examples/example1.jl
@@ -3,13 +3,14 @@ Example 1
 SEIR simulation, visualization, and inference
 Justin Angevaare
 """
+
 using Pathogen, Gadfly, DataFrames, Distributions, ProgressMeter
 
 cd("Desktop/example1")
 
 # Simulate
-init_seq = create_seq(100, 0.25, 0.25, 0.25, 0.25)
-init_var = rand(Uniform(0,10), (2,100))
+init_seq = create_seq(40, 0.25, 0.25, 0.25, 0.25)
+init_var = rand(Uniform(0,10), (2,40))
 
 pop = create_population(init_seq, init_var)
 
@@ -20,7 +21,7 @@ substitution = jc69([0.001])
 
 ratearray = create_ratearray(pop, powerlaw, substitution)
 
-@time while length(pop.timeline[1]) < 300.
+@time while length(pop.timeline[1]) < 5000.
   onestep!(ratearray, pop, powerlaw, latency, recovery, substitution)
 end
 
@@ -36,11 +37,13 @@ actual, obs = surveil(pop, 2.)
 
 obs.infectious
 
+obs.seq
+
 ilm_priors = SEIR_priors(Uniform(1,7), Uniform(2,8), Gamma(0.001), Uniform(0.1,1), Uniform(0.1,1))
 detection_priors = Lag_priors(Uniform(1,3))
 mutation_priors = JC69_priors(Uniform(0,0.003))
 
-ilm_trace, detection_trace, mutation_trace = MCMC(50000, ilm_priors, detection_priors, mutation_priors, obs)
+ilm_trace, detection_trace, mutation_trace = MCMC(1, ilm_priors, detection_priors, mutation_priors, obs, true)
 
 # Tune the transition kernel's covariance matrix
 n = 100
--- src/infer.jl
+++ src/infer.jl
@@ -461,24 +461,25 @@ function initialize(ilm_priors::SEIR_priors, mutation_priors::JC69_priors, detec
   end
 
   if count < limit
-    lp1 += logprior(ilm_priors, ilm_params) + logprior(mutation_priors, mutation_params)
+    lp1 += logprior(ilm_priors, ilm_params) + logprior(detection_priors, detection_params)
     network = propose_network(network_rates, false, debug)
     lp2 = network_loglikelihood(obs, aug, network, jc69([mutation_params[1]]), debug)
-    lp2 += logprior(detection_priors, detection_params)
+    lp2 += logprior(mutation_priors, mutation_params)
 
     while lp1 + lp2 == -Inf && count < limit
       count += 1
       lp1, network_rates = SEIR_loglikelihood(ilm_params[1], ilm_params[2], ilm_params[3], ilm_params[4], ilm_params[5], aug, obs, dist, debug)
-      lp1 += logprior(ilm_priors, ilm_params) + logprior(mutation_priors, mutation_params)
-      network = propose_network(network_rates, false, debug)lp2 = network_loglikelihood(obs, aug, network, jc69([mutation_params[1]]), debug)
-      lp2 += logprior(detection_priors, detection_params)
+      lp1 += logprior(ilm_priors, ilm_params) + logprior(detection_priors, detection_params)
+      network = propose_network(network_rates, false, debug)
+      lp2 = network_loglikelihood(obs, aug, network, jc69([mutation_params[1]]), debug)
+      lp2 += logprior(mutation_priors, mutation_params)
     end
 
-    if count < limit
-      println("Successfully initialized on attempt $count")
+    if count < limit && lp1 + lp2 > -Inf
+      println("Successfully initialized on attempt $count (lp1 = $lp1, lp2 = $lp2)")
       return SEIR_trace([ilm_params[1]], [ilm_params[2]], [ilm_params[3]], [ilm_params[4]], [ilm_params[5]], [aug], Array[network_rates], Array[network], [lp1], [lp2]), Lag_trace([detection_params[1]]), JC69_trace([mutation_params[1]])
     else
-      println("Failed to initialize after $count attempts")
+      println("Failed to initialize after $count attempts (lp1 = $lp1, lp2 = $lp2)")
     end
   end
 end
@@ -642,6 +643,16 @@ function MCMC(n::Int64,
       reject = false
     elseif exp(lp1_proposal - lp1) > rand()
       reject = false
+    end
+
+    if debug
+      if lp1_proposal > lp1
+        println("accepted proposal ($lp1_proposal > $lp1) on $i th iteration")
+      elseif exp(lp1_proposal - lp1) > rand()
+        println("accepted proposal (with probability $(exp(lp1_proposal - lp1)) on $i th iteration)")
+      else
+        println("rejected proposal on $i th iteration")
+      end
     end
 
     if reject
