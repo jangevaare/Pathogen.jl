a827866cdade2ad80fe24d41ce5b6b1c68c94c97
Justin Angevaare <jangevaa@uoguelph.ca>
2015-11-23 15:08:17 -0500

    initialization methods for SIR models

--- src/inference/mcmc.jl
+++ src/inference/mcmc.jl
@@ -28,7 +28,13 @@ end
 """
 Initiate an Trace object by sampling from specified prior distributions
 """
-function initialize(ilm_priors::SEIR_priors, mutation_priors::JC69_priors, detection_priors::Lag_priors, obs::SEIR_observed, limit=500::Int, debug=false::Bool, dist=Euclidean())
+function initialize(ilm_priors::SEIR_priors,
+                    mutation_priors::JC69_priors,
+                    detection_priors::Lag_priors,
+                    obs::SEIR_observed,
+                    limit=500::Int,
+                    debug=false::Bool,
+                    dist=Euclidean())
   count = 0
   lp = -Inf
 
@@ -203,6 +209,182 @@ end
 
 
 """
+Initiate an Trace object by sampling from specified prior distributions
+"""
+function initialize(ilm_priors::SIR_priors,
+                    mutation_priors::JC69_priors,
+                    detection_priors::Lag_priors,
+                    obs::SIR_observed,
+                    limit=500::Int,
+                    debug=false::Bool,
+                    dist=Euclidean())
+  count = 0
+  lp = -Inf
+
+  # Retry initialization until non-negative infinity loglikelihood
+  while (lp == -Inf || lp == Inf) && count < limit
+    count += 1
+    ilm_params = randprior(ilm_priors)
+    mutation_params = randprior(mutation_priors)
+    detection_params = randprior(detection_priors)
+    aug = propose_augment(detection_params[1], obs, debug)
+    lp, network_rates = SIR_loglikelihood(ilm_params[1],
+                                          ilm_params[2],
+                                          ilm_params[3],
+                                          ilm_params[4],
+                                          aug,
+                                          obs,
+                                          debug,
+                                          dist)
+    lp += logprior(ilm_priors,
+                   ilm_params,
+                   debug)
+
+    lp += logprior(detection_priors,
+                   detection_params,
+                   debug)
+    if Inf > lp > -Inf
+      network = propose_network(network_rates,
+                                debug)
+      lp += exposure_network_loglikelihood(network,
+                                           network_rates,
+                                           debug)
+      lp += detection_loglikelihood(detection_params[1],
+                                    aug,
+                                    network,
+                                    obs,
+                                    debug)
+    end
+    if Inf > lp > -Inf
+      lp += logprior(mutation_priors,
+                     mutation_params,
+                     debug)
+    end
+    if Inf > lp > -Inf
+      lp += phylogenetic_network_loglikelihood(obs,
+                                               aug,
+                                               network,
+                                               jc69p([mutation_params[1]]),
+                                               debug)
+    end
+    if Inf > lp > -Inf
+      return SIR_trace([ilm_params[1]],
+                       [ilm_params[2]],
+                       [ilm_params[3]],
+                       [ilm_params[4]],
+                       [aug],
+                       Array[network_rates],
+                       Array[network],
+                       [lp]),
+              Lag_trace([detection_params[1]]),
+              JC69_trace([mutation_params[1]])
+    end
+    @assert(count < limit && Inf > lp > -Inf, "Failed to initialize")
+  end
+end
+
+
+"""
+Initiate an Trace object by sampling from specified prior distributions
+"""
+function initialize(ilm_priors::SIR_priors,
+                    detection_priors::Lag_priors,
+                    obs::SEIR_observed,
+                    limit=500::Int,
+                    debug=false::Bool,
+                    dist=Euclidean())
+  count = 0
+  lp = -Inf
+
+  # Retry initialization until non-negative infinity loglikelihood
+  while (lp == -Inf || lp == Inf) && count < limit
+    count += 1
+    ilm_params = randprior(ilm_priors)
+    detection_params = randprior(detection_priors)
+    aug = propose_augment(detection_params[1],
+                          obs,
+                          debug)
+    lp, network_rates = SIR_loglikelihood(ilm_params[1],
+                                          ilm_params[2],
+                                          ilm_params[3],
+                                          ilm_params[4],
+                                          aug,
+                                          obs,
+                                          debug,
+                                          dist)
+    lp += logprior(ilm_priors, ilm_params, debug)
+    lp += logprior(detection_priors, detection_params)
+    if Inf > lp > -Inf
+      network = propose_network(network_rates, debug)
+      lp += detection_loglikelihood(detection_params[1],
+                                   aug,
+                                   network,
+                                   obs,
+                                   debug)
+      lp += exposure_network_loglikelihood(network, network_rates, debug)
+    end
+    if Inf > lp > -Inf
+      return SIR_trace([ilm_params[1]],
+                       [ilm_params[2]],
+                       [ilm_params[3]],
+                       [ilm_params[4]],
+                       [aug],
+                       Array[network_rates],
+                       Array[network],
+                       [lp]),
+              Lag_trace([detection_params[1]])
+    end
+    @assert(count < limit && Inf > lp > -Inf, "Failed to initialize")
+  end
+end
+
+
+"""
+Initiate an Trace object by sampling from specified prior distributions
+"""
+function initialize(ilm_priors::SIR_priors,
+                    obs::SIR_observed,
+                    limit=500::Int,
+                    debug=false::Bool,
+                    dist=Euclidean())
+  count = 0
+  lp = -Inf
+
+  # Retry initialization until non-negative infinity loglikelihood
+  while (lp == -Inf || lp == Inf) && count < limit
+    count += 1
+    ilm_params = randprior(ilm_priors)
+    aug = propose_augment(obs,
+                          debug)
+    lp, network_rates = SIR_loglikelihood(ilm_params[1],
+                                          ilm_params[2],
+                                          ilm_params[3],
+                                          ilm_params[4],
+                                          aug,
+                                          obs,
+                                          debug,
+                                          dist)
+    lp += logprior(ilm_priors, ilm_params, debug)
+    if Inf > lp > -Inf
+      network = propose_network(network_rates, debug)
+      lp += exposure_network_loglikelihood(network, network_rates, debug)
+    end
+    if Inf > lp > -Inf
+      return SIR_trace([ilm_params[1]],
+                       [ilm_params[2]],
+                       [ilm_params[3]],
+                       [ilm_params[4]],
+                       [aug],
+                       Array[network_rates],
+                       Array[network],
+                       [lp]),
+    end
+    @assert(count < limit && Inf > lp > -Inf, "Failed to initialize")
+  end
+end
+
+
+"""
 A simple function for Metropolis-Hastings rejection using log posteriors
 """
 function MHreject(lp1::Float64, lp2::Float64, debug=false::Bool)
@@ -218,6 +400,7 @@ function MHreject(lp1::Float64, lp2::Float64, debug=false::Bool)
   debug && reject && println("MCMC Proposal rejected")
   return reject
 end
+
 
 function MCMC(n::Int64,
               transition_cov::Array{Float64},
