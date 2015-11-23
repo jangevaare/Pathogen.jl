86df717a2952a67b35c00a6fc4260da3c763d021
Justin Angevaare <jangevaa@uoguelph.ca>
2015-11-23 10:44:35 -0500

    further organize event time augmentation

--- src/inference/eventtimes/seir.jl
+++ src/inference/eventtimes/seir.jl
@@ -4,7 +4,10 @@ Initial augmented data proposal
 * With detection rate, `ν`
 * Proposals made with exponential distributions
 """
-function propose_augment(ρ::Float64, ν::Float64, obs::SEIR_observed, debug=false::Bool)
+function propose_augment(ρ::Float64,
+                         ν::Float64,
+                         obs::SEIR_observed,
+                         debug=false::Bool)
   exposed_augmented = fill(NaN, length(obs.infectious))
   infectious_augmented = fill(NaN, length(obs.infectious))
   removed_augmented = fill(NaN, length(obs.removed))
@@ -27,7 +30,8 @@ Initial augmented data proposal
 * With detection rate, `ν`
 * Proposals made with uniform distributions
 """
-function propose_augment(obs::SEIR_observed, debug=false::Bool)
+function propose_augment(obs::SEIR_observed,
+                         debug=false::Bool)
   exposed_augmented = fill(NaN, length(obs.infectious))
   infectious_augmented = fill(NaN, length(obs.infectious))
   removed_augmented = fill(NaN, length(obs.removed))
@@ -49,7 +53,9 @@ Initial augmented data proposal
 * For SEIR models, with infectivity rate, `ρ`
 * Proposals made with exponential distributions
 """
-function propose_augment(obs::SEIR_observed, debug=false::Bool)
+function propose_augment(ρ::Float64,
+                         obs::SEIR_observed,
+                         debug=false::Bool)
   infectious_augmented = obs.infectious
   removed_augmented = obs.removed
   exposed_augmented = fill(NaN, length(infectious_augmented))
@@ -88,7 +94,11 @@ Proposes new augmented data
 * Proposals made with uniform distributions
 * Generates new event times for a specified `changed_individuals`
 """
-function propose_augment(changed_individuals::Vector{Int64}, network::Array{Bool, 2}, previous_aug::SEIR_augmented, obs::SEIR_observed, debug=false::Bool)
+function propose_augment(changed_individuals::Vector{Int64},
+                         network::Array{Bool, 2},
+                         previous_aug::SEIR_augmented,
+                         obs::SEIR_observed,
+                         debug=false::Bool)
   exposed_augmented = previous_aug.exposed
   infectious_augmented = previous_aug.infectious
   removed_augmented = previous_aug.removed
@@ -112,7 +122,6 @@ function propose_augment(changed_individuals::Vector{Int64}, network::Array{Bool
     aug_order = sample([1,2,3], 3, replace=false)
     for j in aug_order
       # Exposure time augmentation
-      infectious_augmented[i] = rand(Uniform(exposed_augmented[i], minimum([obs.infectious[i]; exposed_augmented[pathway_out[2:end]]])))
       if j == 1
         if length(pathway_in) > 2
           if isnan(obs.removed[pathway_in[2]])
@@ -145,7 +154,11 @@ Proposes new augmented data
 * Proposals made with truncated exponential distributions
 * Generates new event times for a specified amount of `changes`
 """
-function propose_augment(network::Array{Bool, 2}, previous_aug::SEIR_augmented, obs::SEIR_observed, changes=1::Int64, debug=false::Bool)
+function propose_augment(network::Array{Bool, 2},
+                         previous_aug::SEIR_augmented,
+                         obs::SEIR_observed,
+                         changes=1::Int64,
+                         debug=false::Bool)
   exposures = pathwayfrom(0, network, debug)[2:end]
   if changes == 0 || changes > length(exposures)
     changed_individuals = sample(exposures, length(exposures), replace=false)
@@ -165,7 +178,13 @@ Proposes new augmented data
 * Proposals made with truncated exponential distributions
 * Generates new event times for a specified `changed_individuals`
 """
-function propose_augment(changed_individuals::Vector{Int64}, ρ::Float64, ν::Float64, network::Array{Bool, 2}, previous_aug::SEIR_augmented, obs::SEIR_observed, debug=false::Bool)
+function propose_augment(changed_individuals::Vector{Int64},
+                         ρ::Float64,
+                         ν::Float64,
+                         network::Array{Bool, 2},
+                         previous_aug::SEIR_augmented,
+                         obs::SEIR_observed,
+                         debug=false::Bool)
   exposed_augmented = previous_aug.exposed
   infectious_augmented = previous_aug.infectious
   removed_augmented = previous_aug.removed
@@ -220,7 +239,11 @@ Proposes new augmented data
 * Proposals made with truncated exponential distributions
 * Generates new event times for a specified `changed_individuals`
 """
-function propose_augment(changed_individuals::Vector{Int64}, ρ::Float64, network::Array{Bool, 2}, previous_aug::SEIR_augmented, obs::SEIR_observed, debug=false::Bool)
+function propose_augment(changed_individuals::Vector{Int64},
+                         ρ::Float64, network::Array{Bool, 2},
+                         previous_aug::SEIR_augmented,
+                         obs::SEIR_observed,
+                         debug=false::Bool)
   exposed_augmented = previous_aug.exposed
   infectious_augmented = previous_aug.infectious
   removed_augmented = previous_aug.removed
@@ -259,7 +282,11 @@ Proposes augmented data
 * Requires `network` information
 * Proposals made with truncated exponential distributions
 """
-function propose_augment(ρ::Float64, ν::Float64, network::Array{Bool, 2}, obs::SEIR_observed, debug=false::Bool)
+function propose_augment(ρ::Float64,
+                         ν::Float64,
+                         network::Array{Bool, 2},
+                         obs::SEIR_observed,
+                         debug=false::Bool)
   individuals = pathwayfrom(0, network, debug)[2:end]
   exposed_augmented = fill(NaN, length(obs.infectious))
   infectious_augmented = fill(NaN, length(obs.infectious))
@@ -302,7 +329,13 @@ Proposes new augmented data
 * Proposals made with truncated exponential distributions
 * Generates new event times for a specified amount of `changes`
 """
-function propose_augment(ρ::Float64, ν::Float64, network::Array{Bool, 2}, previous_aug::SEIR_augmented, obs::SEIR_observed, changes=1::Int64, debug=false::Bool)
+function propose_augment(ρ::Float64,
+                         ν::Float64,
+                         network::Array{Bool, 2},
+                         previous_aug::SEIR_augmented,
+                         obs::SEIR_observed,
+                         changes=1::Int64,
+                         debug=false::Bool)
   exposures = pathwayfrom(0, network, debug)[2:end]
   if changes == 0 || changes > length(exposures)
     changed_individuals = sample(exposures, length(exposures), replace=false)
@@ -319,7 +352,10 @@ Proposes augmented data
 * Requires `network` information
 * Proposals made with truncated exponential distributions
 """
-function propose_augment(ρ::Float64, network::Array{Bool, 2}, obs::SEIR_observed, debug=false::Bool)
+function propose_augment(ρ::Float64,
+                         network::Array{Bool, 2},
+                         obs::SEIR_observed,
+                         debug=false::Bool)
   individuals = pathwayfrom(0, network, debug)[2:end]
   exposed_augmented = fill(NaN, length(obs.infectious))
   infectious_augmented = fill(NaN, length(obs.infectious))
@@ -349,7 +385,12 @@ Proposes new augmented data
 * Proposals made with truncated exponential distributions
 * Generates new event times for a specified amount of `changes`
 """
-function propose_augment(ρ::Float64, network::Array{Bool, 2}, previous_aug::SEIR_augmented, obs::SEIR_observed, changes=1::Int64, debug=false::Bool)
+function propose_augment(ρ::Float64,
+                         network::Array{Bool, 2},
+                         previous_aug::SEIR_augmented,
+                         obs::SEIR_observed,
+                         changes=1::Int64,
+                         debug=false::Bool)
   exposures = pathwayfrom(0, network, debug)[2:end]
   if changes == 0 || changes > length(exposures)
     changed_individuals = sample(exposures, length(exposures), replace=false)
@@ -364,7 +405,11 @@ end
 Log likelihood for detection rate, `ν`
 * Requires `network` information
 """
-function detection_loglikelihood(ν::Float64, aug::SEIR_augmented, network::Array{Bool, 2}, obs::SEIR_observed, debug=false::Bool)
+function detection_loglikelihood(ν::Float64,
+                                 aug::SEIR_augmented,
+                                 network::Array{Bool, 2},
+                                 obs::SEIR_observed,
+                                 debug=false::Bool)
   ll = 0.
   individuals = find(!isnan(obs.infectious))
   exposed_augmented = aug.exposed
