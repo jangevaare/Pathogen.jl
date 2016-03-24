1adc062f075658cad90fe378744513eb00745168
Justin Angevaare <jangevaa@uoguelph.ca>
2016-03-24 17:28:50 -0400

    work on package structuring

---
+++ src/risks.jl
@@ -0,0 +1,25 @@
+"""
+A collection of the risk functions required for simulation and inference. These
+risk functions are parameterized by a `RiskParameters` object
+"""
+type RiskFunctions
+  susceptibility::Function
+  transmissibility::Function
+  infectivity::Function
+  sparks::Function
+  latency::Function
+  removal::Function
+end
+
+
+"""
+Parameter vectors for the `RiskFunctions`
+"""
+type RiskParameters
+  susceptibility::Vector{Float64}
+  transmissibility::Vector{Float64}
+  infectivity::Vector{Float64}
+  sparks::Vector{Float64}
+  latency::Vector{Float64}
+  removal::Vector{Float64}
+end
---
+++ src/simulate.jl
@@ -0,0 +1,10 @@
+"""
+Simulate events and a transmission tree for a
+phylodynamic individual level model of infectious disease
+"""
+function simulate(n::Int64,
+                  population::DataFrame,
+                  risk_funcs::RiskFunctions,
+                  risk_params::RiskParameters)
+                  # TODO
+end
