ac3cbee87fb8e7f1f257f992a7196a9ffd169e1a
Justin Angevaare <jangevaa@uoguelph.ca>
2016-09-30 14:19:35 -0400

    bugfix

--- src/inference/risks.jl
+++ src/inference/risks.jl
@@ -62,22 +62,22 @@ function logprior(riskparams::RiskParameters,
                   riskpriors::RiskParameterPriors)
   lp = 0.
   for i = 1:length(riskparams.sparks)
-    lp += loglikelihood(riskpriors.sparks[i], riskparams.sparks[i])
+    lp += loglikelihood(riskpriors.sparks[i], [riskparams.sparks[i]])
   end
   for i = 1:length(riskparams.susceptibility)
-    lp += loglikelihood(riskpriors.susceptibility[i], riskparams.susceptibility[i])
+    lp += loglikelihood(riskpriors.susceptibility[i], [riskparams.susceptibility[i]])
   end
   for i = 1:length(riskparams.transmissibility)
-    lp += loglikelihood(riskpriors.transmissibility[i], riskparams.transmissibility[i])
+    lp += loglikelihood(riskpriors.transmissibility[i], [riskparams.transmissibility[i]])
   end
   for i = 1:length(riskparams.infectivity)
-    lp += loglikelihood(riskpriors.infectivity[i], riskparams.infectivity[i])
+    lp += loglikelihood(riskpriors.infectivity[i], [riskparams.infectivity[i]])
   end
   for i = 1:length(riskparams.latency)
-    lp += loglikelihood(riskpriors.latency[i], riskparams.latency[i])
+    lp += loglikelihood(riskpriors.latency[i], [riskparams.latency[i]])
   end
   for i = 1:length(riskparams.removal)
-    lp += loglikelihood(riskpriors.removal[i], riskparams.removal[i])
+    lp += loglikelihood(riskpriors.removal[i], [riskparams.removal[i]])
   end
   return lp
 end
