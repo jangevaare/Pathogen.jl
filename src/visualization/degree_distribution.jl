d064a2636fcb748110b3cddf2fb65fcc207ba7b5
Justin Angevaare <jangevaa@uoguelph.ca>
2020-04-22 21:04:21 -0400

    Degree distribution fix

--- src/visualization/degree_distribution.jl
+++ src/visualization/degree_distribution.jl
@@ -1,6 +1,6 @@
 function _outdegree(x::TN) where {TN <: Union{TNDistribution, TransmissionNetwork}}
   infected = [x.external[i] > 0 || any(x.internal[:, i] .> 0) for i = 1:individuals(x)]
-  return sum(tnp.internal[infected, infected], dims=2)
+  return sum(x.internal[infected, infected], dims=2)
 end
 
 @recipe function plot(x::TN) where {TN <: Union{TNDistribution, TransmissionNetwork}}
