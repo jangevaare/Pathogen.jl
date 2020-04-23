145f297ddace0881218c19563aa58d355ec4016a
Justin Angevaare <jangevaa@uoguelph.ca>
2020-04-22 20:48:46 -0400

    Add recipe for degree distribution histogram

---
+++ src/visualization/degree_distribution.jl
@@ -0,0 +1,12 @@
+function _outdegree(x::TN) where {TN <: Union{TNDistribution, TransmissionNetwork}}
+  infected = [x.external[i] > 0 || any(x.internal[:, i] .> 0) for i = 1:individuals(x)]
+  return sum(tnp.internal[infected, infected], dims=2)
+end
+
+@recipe function plot(x::TN) where {TN <: Union{TNDistribution, TransmissionNetwork}}
+  seriestype := :histogram
+  legend --> :none
+  xlab --> "Out degree"
+  ylab --> "Frequency"
+  _outdegree(x)
+end
