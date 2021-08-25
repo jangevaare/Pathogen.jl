d37dfb0705b83ad8ec4b48f6409891fb9193a333
Justin Angevaare <jangevaa@uoguelph.ca>
2021-08-25 00:03:25 -0400

    change `labels` to `label` in plot recipe

--- src/visualization/trace.jl
+++ src/visualization/trace.jl
@@ -2,7 +2,7 @@
   xguide --> "Iteration"
   yguide --> "Value"
   legend --> :none
-  labels --> permutedims(_parameters(x[1]))
+  label --> permutedims(_parameters(x[1]))
   convert(Array{Float64, 2}, x)
 end
 
@@ -11,6 +11,6 @@ end
   xguide --> "Iteration"
   yguide --> "Value"
   legend --> :none
-  labels --> permutedims(_parameters(x[1]))
+  label --> permutedims(_parameters(x[1]))
   iter, convert(Array{Float64, 2}, x[iter])
 end
